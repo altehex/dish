4a53965c06364887bc60f99181beea5fe944af93
altehex <99411254+altehex@users.noreply.github.com>
2023-01-10 19:38:23 +0900

    Update lib.hpp

--- lib.hpp
+++ lib.hpp
@@ -7,7 +7,6 @@
 #include <iostream>
 #include <string>
 
-#include "define.hpp"
 #include "parser.hpp"
 #include "ryml.hpp"
 
@@ -22,18 +21,51 @@ using namespace std;
 // |
 // |+--+--+--+--+--+--+--+--+--+--+--+--
 
-template<class T>
-union result {
-    T result;
-    int error;
-};
-
+ryml::Tree no_dish_yml () 
+{
+    cout << "Can't find 'dish.yml'.\n" <<
+    "Using the 'dish/default.yml' file.\n";
+    return parse_file("dish/default.yml");
+}
 
 //
 // dish.yml
 //----------------------
 const ryml::Tree config = (fopen("dish.yml", "rb") == nullptr) ? 
-    parse_file("dish/default.yml") : 
+    no_dish_yml() : 
     parse_file("dish.yml");
 
 
+
+void fork(string command) 
+{
+    STARTUPINFO si;
+    PROCESS_INFORMATION pi;
+
+//    if (command == "quit" || 
+  //      command == "exit") return -1;
+
+    if ( !CreateProcessA(
+        NULL,
+        &*(command.begin()),
+        NULL,
+        NULL,
+        false,
+        0,
+        NULL,
+        NULL,
+        &si,
+        &pi ) 
+    ) {
+        cout << "Failed to start a process\n" <<
+                "Error code: " << GetLastError() << '\n';
+    } else {
+        WaitForSingleObject(
+            pi.hProcess,
+            INFINITE
+        );
+
+        CloseHandle(pi.hProcess);
+        CloseHandle(pi.hThread);
+    }
+}
